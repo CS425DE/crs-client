ef3bc3d08de9367cd8ad0df3cd90df8c8cc089ce
thpchandrika <thpchandrika@gmail.com>
2023-10-16 22:10:14 -0500

    initial commit

--- src/pages/admin/DiscountsList.jsx
+++ src/pages/admin/DiscountsList.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import DiscountService from "../../API/DiscountService";
 import MyButton from "../../components/UI/button/MyButton";
 
-
 const DiscountsList = () => {
   const [discounts, setDiscounts] = useState([]);
   const navigate = useNavigate();
@@ -19,50 +18,56 @@ const DiscountsList = () => {
 
   function handleEdit(id) {
     navigate(`/discounts/${id}`);
-}
+  }
 
-async function handleDelete(id) {
+  async function handleDelete(id) {
     await DiscountService.deletById(id);
     navigate("/discounts");
-}
+  }
 
   return (
     <div className="content1">
-     <div>
-      <h1>Discounts</h1>
-      <Link to="/discounts/new"><MyButton>Create New Discount</MyButton></Link>
-    </div>
+      <div>
+        <h1>Discounts</h1>
+        <Link to="/discounts/new">
+          <MyButton>Create New Discount</MyButton>
+        </Link>
+      </div>
       <table className="table table-striped">
         <thead>
           <tr>
-              <th>#</th>
-              <th>Title</th>
-              <th>Code</th>
-              <th>Description</th>
-              <th>Discount Percentage</th>
-              <th>Vehicle Make</th>
-              <th>Actions</th>
+            <th>#</th>
+            <th>Title</th>
+            <th>Code</th>
+            <th>Description</th>
+            <th>Discount Percentage</th>
+            <th>Vehicle Make</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
           {discounts.map((discount, index) => (
             <tr key={index}>
-                <td>{index+1}</td>
-                <td>{discount.title}</td>
-                <td>{discount.code}</td>
-                <td>{discount.description}</td>
-                <td>{discount.percentage}%</td>
-                <td>{discount.vehicleMake}</td>
-                <td>
-                    <MyButton onClick={() => handleEdit(discount.discountId)}>Edit</MyButton>
-                    <MyButton onClick={() => handleDelete(discount.discountId)}>Delete</MyButton>
-                </td>
+              <td>{index + 1}</td>
+              <td>{discount.title}</td>
+              <td>{discount.code}</td>
+              <td>{discount.description}</td>
+              <td>{discount.percentage}%</td>
+              <td>{discount.vehicleMake}</td>
+              <td>
+                <MyButton onClick={() => handleEdit(discount.discountId)}>
+                  Edit
+                </MyButton>
+                <MyButton onClick={() => handleDelete(discount.discountId)}>
+                  Delete
+                </MyButton>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
 export default DiscountsList;
